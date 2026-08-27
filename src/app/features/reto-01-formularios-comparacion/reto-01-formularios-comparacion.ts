ffdda3c1eeba4a06c6ab75ba45c0cd48e723e24b
Javier Flores <jffc.dev@gmail.com>
2026-08-26 21:40:23 -0500

    finalizando reto 01

--- src/app/features/reto-01-formularios-comparacion/reto-01-formularios-comparacion.ts
+++ src/app/features/reto-01-formularios-comparacion/reto-01-formularios-comparacion.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-reto-01-formularios-comparacion',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule, JsonPipe, ReactiveFormsModule],
   templateUrl: './reto-01-formularios-comparacion.html',
 })
-export class Reto01FormulariosComparacion {}
+export class Reto01FormulariosComparacion {
+  nombre = signal('')
+  email = signal('')
+
+  miFormulario = new FormGroup({
+    nombre: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email, Validators.maxLength(20)])
+  })
+}
