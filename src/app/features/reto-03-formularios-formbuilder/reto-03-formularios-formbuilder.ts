4f7ecb7e0d449395517a2f73b8bcc13b9f7eee72
Javier Flores <jffc.dev@gmail.com>
2026-08-26 22:36:33 -0500

    agregando cambios del reto 03

--- src/app/features/reto-03-formularios-formbuilder/reto-03-formularios-formbuilder.ts
+++ src/app/features/reto-03-formularios-formbuilder/reto-03-formularios-formbuilder.ts
@@ -1,9 +1,41 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormArray, FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-reto-03-formularios-formbuilder',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule, JsonPipe],
   templateUrl: './reto-03-formularios-formbuilder.html',
 })
-export class Reto03FormulariosFormbuilder {}
+export class Reto03FormulariosFormbuilder {
+  private builder = inject(FormBuilder)
+
+  miFormulario = this.builder.group({
+    nombre: [''],
+    email: [''],
+
+    direccion: this.builder.group({
+      calle: [''],
+      ciudad: [''],
+    }),
+
+    telefonos: this.builder.array([this.builder.control('')])
+  })
+
+  get telefonos(): FormArray {
+    return this.miFormulario.get('telefonos') as FormArray
+  }
+
+  agregarTelefono(){
+    if(this.telefonos.length < 3){
+      (this.miFormulario.get('telefonos') as FormArray).push(this.builder.control(''))
+    }
+  }
+
+  quitarTelefono(index: number){
+
+    (this.miFormulario.get('telefonos') as FormArray).removeAt(index)
+    console.log(index)
+  }
+}
