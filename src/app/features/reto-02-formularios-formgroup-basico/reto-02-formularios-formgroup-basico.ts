e74cc6f94dea044abb0e85a09b7a6c9b563927eb
Javier Flores <jffc.dev@gmail.com>
2026-08-26 21:53:15 -0500

    finalizando reto 02

--- src/app/features/reto-02-formularios-formgroup-basico/reto-02-formularios-formgroup-basico.ts
+++ src/app/features/reto-02-formularios-formgroup-basico/reto-02-formularios-formgroup-basico.ts
@@ -1,9 +1,30 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-reto-02-formularios-formgroup-basico',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule, JsonPipe],
   templateUrl: './reto-02-formularios-formgroup-basico.html',
 })
-export class Reto02FormulariosFormgroupBasico {}
+export class Reto02FormulariosFormgroupBasico {
+  miFormulario = new FormGroup({
+    nombre: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email, Validators.maxLength(20)])
+  })
+
+  ngOnInit(): void {
+    this.miFormulario.valueChanges.subscribe((valor) => {
+      console.log(valor)
+    })
+  }
+
+  patchValue(){
+    this.miFormulario.patchValue({nombre: 'Javier', email: 'javier@gmail.com' })
+  }
+
+  reset(){
+    this.miFormulario.reset()
+  }
+}
