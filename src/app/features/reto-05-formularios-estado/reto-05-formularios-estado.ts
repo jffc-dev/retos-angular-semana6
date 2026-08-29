4f9ad0ae1d9d17981214deca87ce41f573b3c240
Javier Flores <jffc.dev@gmail.com>
2026-08-28 21:40:11 -0500

    agregar validacion de estados de campo

--- src/app/features/reto-05-formularios-estado/reto-05-formularios-estado.ts
+++ src/app/features/reto-05-formularios-estado/reto-05-formularios-estado.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-reto-05-formularios-estado',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule, JsonPipe],
   templateUrl: './reto-05-formularios-estado.html',
 })
-export class Reto05FormulariosEstado {}
+export class Reto05FormulariosEstado {
+  private builder = inject(FormBuilder)
+
+  miFormulario = this.builder.group({
+    email: ['', [Validators.required, Validators.email]],
+  })
+
+  get email(){
+    return this.miFormulario.get('email')!
+  }
+}
