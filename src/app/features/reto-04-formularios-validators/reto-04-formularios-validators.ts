ce32cbc1234d65e6fc1d362210c66d9f9d0d6cdb
Javier Flores <jffc.dev@gmail.com>
2026-08-28 21:26:52 -0500

    agregar validacion sincrona

--- src/app/features/reto-04-formularios-validators/reto-04-formularios-validators.ts
+++ src/app/features/reto-04-formularios-validators/reto-04-formularios-validators.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-reto-04-formularios-validators',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule, JsonPipe],
   templateUrl: './reto-04-formularios-validators.html',
 })
-export class Reto04FormulariosValidators {}
+export class Reto04FormulariosValidators {
+  private builder = inject(FormBuilder)
+
+  miFormulario = this.builder.group({
+    nombre: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    usuario: ['', [Validators.minLength(4), Validators.maxLength(12), Validators.required]],
+    numeroTarjeta: ['', [Validators.required,Validators.pattern(/^\d{16}$/)]]
+  })
+}
