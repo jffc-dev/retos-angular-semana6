1f9844ff12f93d2be7640771251a004b5452085e
Javier Flores <jffc.dev@gmail.com>
2026-08-28 22:03:54 -0500

    agregar mensajes de error

--- src/app/features/reto-06-formularios-errores/reto-06-formularios-errores.ts
+++ src/app/features/reto-06-formularios-errores/reto-06-formularios-errores.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-reto-06-formularios-errores',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule, JsonPipe],
   templateUrl: './reto-06-formularios-errores.html',
 })
-export class Reto06FormulariosErrores {}
+export class Reto06FormulariosErrores {
+  private builder = inject(FormBuilder)
+
+  miFormulario = this.builder.group({
+    email: ['', [Validators.required, Validators.email]],
+    contrasenia: ['', [Validators.required, Validators.minLength(8)]],
+  })
+
+  get email(){
+    return this.miFormulario.get('email')!
+  }
+
+  get contrasenia(){
+    return this.miFormulario.get('contrasenia')!
+  }
+}
